54eb9677289e5e58c162bd578c4674ee731c48fe
Elliott Sprehn <esprehn@gmail.com>
2012-05-08 17:27:55 -0700

    Upgrade to Angular 1.0RC8

--- js/presentation.js
+++ js/presentation.js
@@ -1,18 +1,19 @@
+(function(module) {
 
-function PresentationController($location, keyboard) {
-  var scope = this;
+module.controller('PresentationController', PresentationController);
+function PresentationController($scope, $defer, $location, keyboard) {
   var RIGHT_ARROW = 39;
   var LEFT_ARROW = 37;
 
   keyboard.on(RIGHT_ARROW, function() {
-    scope.activeSlide++;
+    $scope.activeSlide++;
   });
 
   keyboard.on(LEFT_ARROW, function() {
-    scope.activeSlide--;
+    $scope.activeSlide--;
   });
 
-  scope.$watch('activeSlide', function(scope, value) {
+  $scope.$watch('activeSlide', function(value) {
     if (value == -1) {
       $location.url('');
     } else if (value > -1) {
@@ -20,48 +21,48 @@ function PresentationController($location, keyboard) {
     }
   });
 
-  scope.$watch(function() { return $location.url(); }, function(scope, value) {
+  $scope.$watch(function() { return $location.url(); }, function(value) {
     var match = /\/slides\/(\d+)/.exec(value);
     if (match) {
-      scope.activeSlide = parseInt(match[1], 10) - 1;
+      $scope.activeSlide = parseInt(match[1], 10) - 1;
     } else if (value == '/slides/end') {
-      scope.activeSlide = scope.totalSlides;
+      $scope.activeSlide = scope.totalSlides;
     }
   });
 
-  scope.isInsideDeck = function() {
+  $scope.isInsideDeck = function() {
     return !this.isBefore() && !this.isAfter();
   };
 
-  scope.isBefore = function() {
-    return scope.activeSlide < 0;
+  $scope.isBefore = function() {
+    return $scope.activeSlide < 0;
   };
 
-  scope.isAfter = function() {
-    return scope.activeSlide >= scope.totalSlides;
+  $scope.isAfter = function() {
+    return $scope.activeSlide >= $scope.totalSlides;
   };
-}
+};
 
-angular.service('keyboard', function() {
-  var scope = this;
-  return {
-    on: function(keyCode, callback) {
-      $(window).keydown(function(e) {
-        if (e.keyCode == keyCode) {
-          scope.$apply(callback);
-        }
-      });
-    }
+
+module.service('keyboard', KeyboardService);
+function KeyboardService($rootScope) {
+  // TODO(esprehn): This implementation leaks since we never remove the listener
+  // even if the controller has been $destroy'ed.
+  this.on = function(keyCode, callback) {
+    $(window).keydown(function(e) {
+      if (e.keyCode == keyCode) {
+        $rootScope.$apply(callback);
+      }
+    });
   };
-});
+};
 
-angular.widget('deck', function(element) {
-  this.directives(true);
-  this.descend(true);
-  return function() {
+
+module.directive('deck', function() {
+  function link($scope, element, attrs) {
     var slides = element.find('slide');
-    var name = element.attr('current');
-    var total = element.attr('total');
+    // var name = atrs.current;
+    // var total = atrs.total;
 
     function restack() {
       slides.each(function(i, slide) {
@@ -74,9 +75,10 @@ angular.widget('deck', function(element) {
 
     restack();
 
-    this.$eval(total + ' = ' + slides.length);
+    $scope.total(slides.length);
+    $scope.current(-1);
 
-    this.$watch(name, function(scope, value) {
+    $scope.$watch('current()', function(value) {
       slides.each(function(i, slide) {
         $(slide).removeClass('previous current next');
         if (i < value) {
@@ -89,20 +91,36 @@ angular.widget('deck', function(element) {
       });
 
       if (value < -1 || isNaN(value)) {
-        scope.$eval(name + ' = -1');
+        $scope.current(-1);
       } else if (value > slides.length) {
-        scope.$eval(name + ' = ' + slides.length);
+        $scope.current(slides.length);
+      } else {
+        restack();
       }
-
-      restack();
     });
+  };
+
+  return {
+    restrict: 'E', // Allow it on elements
+    scope: {
+      current: 'accessor',
+      total: 'accessor'
+    },
+    link: link
   };
 });
 
-angular.widget('@slide:code', function(value, element) {
-  element.addClass('brush: js; toolbar: false;');
-  if (value != 'js') {
-    element.addClass('html-script: true;');
-  }
-  element.attr('ng:non-bindable', '');
+
+module.directive('slideCode', function() {
+  return function(scope, element, attrs) {
+    var value = attrs.slideCode;
+
+    element.addClass('brush: js; toolbar: false;');
+    if (value != 'js') {
+      element.addClass('html-script: true;');
+    }
+    element.attr('ng-non-bindable', '');
+  };
 });
+
+})(angular.module('PresentationModule', []));
